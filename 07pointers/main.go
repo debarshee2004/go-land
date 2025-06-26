00910a0daa93f90143528e0947471cdf1b8b8227
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2025-06-26 16:33:44 +0530

    Updated pointers notes

--- 07pointers/main.go
+++ 07pointers/main.go
@@ -1,24 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// Topic 06 - Pointers in Go
 
 func main() {
 	fmt.Println("Welcome to the Pointers package!")
 
-	// var ptr *int
-	// fmt.Println("Value of pointer is:", ptr)
-	// fmt.Printf("Type of pointer is: %T\n", ptr)
+	// ----------------------------
+	// Example 1: Declaring a nil pointer
+	fmt.Println("\nExample 1: Nil pointer declaration")
+	var ptr *int                                        // Declares a pointer to an int, but it points to nothing (nil)
+	fmt.Println("Value of uninitialized pointer:", ptr) // nil
+	fmt.Printf("Type of ptr: %T\n", ptr)
 
+	// ----------------------------
+	// Example 2: Using pointer to get and set values
+	fmt.Println("\nExample 2: Assigning address to pointer")
+	value := 100
+	ptr = &value // Assigns the address of value to ptr
+	fmt.Println("Address stored in ptr:", ptr)
+	fmt.Println("Value pointed to by ptr:", *ptr) // Dereferencing
+
+	*ptr = 200 // Changing value at the address
+	fmt.Println("Updated value through ptr:", value)
+
+	// ----------------------------
+	// Example 3: Pointers with strings
+	fmt.Println("\nExample 3: Pointer to a string")
 	text := "initial"
 	strptr := &text
+	fmt.Println("Value of text:", text)
+	fmt.Println("Address of text (strptr):", strptr)
+	fmt.Println("Value at address of strptr:", *strptr)
 
-	// fmt.Println("Value of text is:", text)
-	// fmt.Println("Value of pointer to text is:", strptr)
-	// fmt.Printf("Type of strptr is: %T\n", strptr)
+	// ----------------------------
+	// Example 4: Double pointer (pointer to a pointer)
+	fmt.Println("\nExample 4: Double pointer")
+	doublePnt := &strptr // strptr is *string, so &strptr is **string
+	fmt.Println("doublePnt (type **string):", doublePnt)
+	fmt.Println("Value pointed by doublePnt (*doublePnt):", *doublePnt)       // value of strptr
+	fmt.Println("Value pointed by *(*doublePnt) (**doublePnt):", **doublePnt) // value of text
 
-	doublePnt := &strptr
-	fmt.Println("Value of double pointer is:", doublePnt)
-	fmt.Println("Value pointed by double pointer is:", *doublePnt)
-	fmt.Println("Value pointed by the pointer pointed by double pointer is:", **doublePnt)
-	fmt.Println("Value of double pointer is:", doublePnt)
+	// ----------------------------
+	// Example 5: Function with pointer parameter
+	fmt.Println("\nExample 5: Passing pointer to function")
+	num := 5
+	fmt.Println("Before function call, num =", num)
+	increment(&num) // pass pointer
+	fmt.Println("After function call, num =", num)
+
+	// ----------------------------
+	// Example 6: Returning pointer from function
+	fmt.Println("\nExample 6: Returning pointer from function")
+	ptrFromFunc := newInt()
+	fmt.Println("Value from returned pointer:", *ptrFromFunc)
+}
+
+// ----------------------------
+// Function that modifies value through pointer
+func increment(n *int) {
+	*n = *n + 1
+}
+
+// Function that returns a pointer to an int
+func newInt() *int {
+	i := 42
+	return &i
 }
