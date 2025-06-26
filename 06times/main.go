22937f0a872d0f61a48cb60328092af1cf190b1d
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2025-06-26 16:31:57 +0530

    Updated time notes

--- 06times/main.go
+++ 06times/main.go
@@ -8,24 +8,52 @@ import (
 func main() {
 	fmt.Println("Welcome to the Times package!")
 
+	// ----------------------------
+	// Example 1: Getting the current time
 	presentTime := time.Now()
-	fmt.Println("Current time:", presentTime)
-	fmt.Printf("Current time type: %T\n", presentTime)
+	fmt.Println("\nExample 1: Current Time")
+	fmt.Println("Current time (raw):", presentTime)
+	fmt.Printf("Type of presentTime: %T\n", presentTime)
 
-	// Formatting time
-	formattedTime := presentTime.Format("01-02-2006 15:04:05 Monday")
+	// ----------------------------
+	// Example 2: Formatting time
+	fmt.Println("\nExample 2: Formatting Time")
+	// Layout must be "Mon Jan 2 15:04:05 MST 2006"
+	formattedTime := presentTime.Format("01-02-2006 15:04:05 Monday") // MM-DD-YYYY HH:MM:SS Day
 	fmt.Println("Formatted time:", formattedTime)
 
-	// Parsing time
+	// ----------------------------
+	// Example 3: Parsing time string into time.Time
+	fmt.Println("\nExample 3: Parsing Time")
 	parsedTime, err := time.Parse("01-02-2006 15:04:05 Monday", formattedTime)
 	if err != nil {
 		fmt.Println("Error parsing time:", err)
 	} else {
-		fmt.Println("Parsed time:", parsedTime)
+		fmt.Println("Parsed time object:", parsedTime)
 	}
 
-	// Creating a specific time
-	createTime := time.Date(2023, time.December, 25, 14, 30, 0, 0, time.UTC)
-	fmt.Println("Created time:", createTime)
-	fmt.Printf("Created time type: %T\n", createTime)
+	// ----------------------------
+	// Example 4: Creating a specific time (e.g., for scheduling)
+	fmt.Println("\nExample 4: Creating a Specific Time")
+	customTime := time.Date(2023, time.December, 25, 14, 30, 0, 0, time.UTC)
+	fmt.Println("Custom created time:", customTime)
+	fmt.Printf("Type of customTime: %T\n", customTime)
+
+	// ----------------------------
+	// Example 5: Duration between two times
+	fmt.Println("\nExample 5: Calculating Time Duration")
+	start := time.Now()
+	time.Sleep(2 * time.Second) // Simulate delay
+	end := time.Now()
+	duration := end.Sub(start)
+	fmt.Printf("Time elapsed: %v\n", duration)
+
+	// ----------------------------
+	// Example 6: Adding/Subtracting time
+	fmt.Println("\nExample 6: Time Arithmetic")
+	tomorrow := presentTime.Add(24 * time.Hour)
+	fmt.Println("Tomorrow will be:", tomorrow)
+
+	yesterday := presentTime.Add(-24 * time.Hour)
+	fmt.Println("Yesterday was:", yesterday)
 }
